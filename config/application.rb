18cd364cebfd0eac723d761e888220b2617e840d
hatabo <straightener0201@hotmail.co.jp>
2024-05-21 22:09:57 +0900

    i18nを使用したRailsの多言語化対応

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,7 @@ module V3BasicRailsBasic
       g.text_framework false
       g.skip_routse true
     end
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
